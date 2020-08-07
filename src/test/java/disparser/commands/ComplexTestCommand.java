6c2a03099f3a9c54a7bf8694ed10da977768d087
SmellyModder <smellysocks543@gmail.com>
2020-08-07 00:46:55 -0400

    Added Either Argument to Complex Test Command

--- src/test/java/disparser/commands/ComplexTestCommand.java
+++ src/test/java/disparser/commands/ComplexTestCommand.java
@@ -2,27 +2,32 @@ package disparser.commands;
 
 import disparser.Command;
 import disparser.CommandContext;
+import disparser.EitherArgument;
+import disparser.EitherArgument.Either;
 import disparser.arguments.jda.TextChannelArgument;
 import disparser.arguments.primitive.CharArgument;
 import disparser.arguments.primitive.FloatArgument;
 import disparser.arguments.primitive.IntegerArgument;
+import disparser.arguments.primitive.LongArgument;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class ComplexTestCommand extends Command {
 
 	public ComplexTestCommand() {
-		super("complex", TextChannelArgument.get().asOptional(), IntegerArgument.get(), FloatArgument.get(), CharArgument.get().asOptional());
+		super("complex", TextChannelArgument.get().asOptional(), EitherArgument.of(IntegerArgument.get(), LongArgument.get()), FloatArgument.get(), CharArgument.get().asOptional());
 	}
 
 	@Override
 	public void processCommand(CommandContext context) {
 		GuildMessageReceivedEvent event = context.getEvent();
 		TextChannel channel = context.getParsedResultOrElse(0, event.getChannel());
-		int integer = context.getParsedResult(1);
+		Either<Integer, Long> either = context.getParsedResult(1);
 		float afloat = context.getParsedResult(2);
 		char character = context.getParsedResultOrElse(3, 'X');
-		channel.sendMessage("#" + integer + " got a " + afloat + " accuracy score " + "for the character " + character).queue();
+		channel.sendMessage(either.convertTo((eitherIn) -> {
+			return eitherIn.first != null ? "#" + either.first : event.getGuild().getMemberById(eitherIn.second).getAsMention();
+		}) + " got a " + afloat + " accuracy score " + "for the character " + character).queue();
 	}
 
 }
