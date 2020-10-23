d0e184c1c3754c4b06bf711252db58f8178628a8
SmellyModder <smellysocks543@gmail.com>
2020-10-22 23:25:05 -0400

    Added package-infos for the new packages

---
+++ src/main/java/net/smelly/disparser/concurrent/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Concurrent related types package for Disparser.
+ * <p>There are other concurrent related types in Disparser that are not in this package, as this package is solely for 'pure' concurrent-related types.</p>
+ *
+ * @author Luke Tonon
+ * @version 1.4.0
+ * @see net.smelly.disparser.concurrent.DisparsingThreadFactory
+ */
+package net.smelly.disparser.concurrent;
---
+++ src/main/java/net/smelly/disparser/properties/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This packages contains all types internally used for command properties.
+ * <p>The command property system allows for dynamic value mapping for {@link net.smelly.disparser.Command}s and simple annotation processing for annotations, all while remaining thread-safe.</p>
+ *
+ * @author Luke Tonon
+ * @version 1.4.0
+ */
+package net.smelly.disparser.properties;
