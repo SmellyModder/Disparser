af68777a1025ac6c7c25f8dd11eff9ea5d80776a
SmellyModder <smellysocks543@gmail.com>
2020-10-30 23:37:07 -0400

    Added package infos for the new packages

---
+++ src/main/java/net/smelly/disparser/context/handlers/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the built-in classes used for handling commands and processing them with {@link net.smelly.disparser.context.CommandContext}.
+ *
+ * @author Luke Tonon
+ * @version 1.4.0
+ */
+package net.smelly.disparser.context.handlers;
---
+++ src/main/java/net/smelly/disparser/context/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains the built-in classes used for {@link net.smelly.disparser.context.CommandContext} creation and processing.
+ * <p>
+ * Internal Packages:
+ * <p>
+ * {@link net.smelly.disparser.context.handlers}
+ * </p>
+ *
+ * @author Luke Tonon
+ * @version 1.4.0
+ */
+package net.smelly.disparser.context;
